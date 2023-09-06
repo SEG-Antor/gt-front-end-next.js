c447580f66f877f528ad91fe73c89d4a1265ff25
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-09-07 05:40:24 +0600

    landing page done upto preowned and refubrished buttton

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from 'next/link'
-import Admin from './admin-secret/page'
+import "./landingpage.css"
 
 export default function Home() {
   return (
@@ -10,7 +10,48 @@ export default function Home() {
       <hr></hr>
       <Link href="admin-secret"> Go to admin secret</Link> */}
 
-      <h4>main page carosel comes here </h4>
+      <img className='w-full' src='/staticImages/banner.png' alt='banner' />
+
+      <div className='logos'>
+        <div>
+          <img src="/logos/warrenty.png" alt='warrenty' />
+          <span> 1 Year Warrenty</span>
+        </div>
+
+        <div>
+          <img src="/logos/giftcard.png" alt='giftcard' />
+          <span> GT Gift Card</span>
+        </div>
+
+        <div>
+          <img src="/logos/fastdelivery.png" alt='fastdelivery' />
+          <span> Fast Delivery</span>
+        </div>
+
+        <div>
+          <img src="/logos/trustedseller.png" alt='trustedseller' />
+          <span> Trusted Seller</span>
+        </div>
+
+        <div>
+          <img src="/logos/klarna.png" alt='klarna' />
+          <span> Payment Plan</span>
+        </div>
+
+        <div>
+          <img src="/logos/return.png" alt='return' />
+          <span> 30 Days Return</span>
+        </div>
+      </div>
+
+      <Link href="top-seller">
+        <img className='banner-2' src="/staticImages/second-banner.png" alt="second banner" />
+      </Link>
+
+      <div className='pre-and-refub'>
+        <Link href="pre-owned"> PRE-OWNED</Link>
+        <Link href="refubrished"> REFUBRISHED</Link>
+      </div>
     </main>
   )
 }
